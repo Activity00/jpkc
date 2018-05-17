c5c12184ec3026f7ff153edfde4875052990f4b6
Activity00 <1032662429@qq.com>
2018-05-17 15:14:14 +0800

    code share

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//单机左侧按钮
+function clickMenu(item_num) {
+ 	/*var span_content=document.getElementById("span_content");
+ 	switch(item_num)
+ 	{
+ 		case 1:
+           span_content.innerHTML="姓名：李菲</br> "+
+           "职称：副教授";
+ 		   break;
+ 		case 2:
+ 		   break;
+
+ 		case 3:
+ 		    break;
+ 		case 4:
+ 		    break;
+ 		case 5:break;
+ 		case 6:break;       
+ 	}*/
+ }
